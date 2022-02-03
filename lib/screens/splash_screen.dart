c96b22e890a5a5d2f85c3773a2e99a661141c368
rachit <rachau76@gmail.com>
2022-02-03 10:52:57 +0530

    added animation on splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,48 @@
 import 'package:blitzz/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 670),
+    vsync: this,
+  );
+  late final Animation<Offset> _offsetAnimation1 = Tween<Offset>(
+    begin: const Offset(0.0, -1.0),
+    end: const Offset(0.0, 0.0),
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  ));
+  late final Animation<Offset> _offsetAnimation2 = Tween<Offset>(
+    begin: const Offset(0.0, 1.0),
+    end: const Offset(0.0, 0.0),
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  ));
+
+  @override
+  void initState() {
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,59 +54,45 @@ class SplashScreen extends StatelessWidget {
           Navigator.pushNamed(context, '/login_page');
         },
         child: Scaffold(
+            backgroundColor: Colors.white,
             body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Row(
-            children: [
-              Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.55,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              "THINK\n",
-                              style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    color: _theme.secondaryColor,
-                                    letterSpacing: .5,
-                                    fontSize: 30),
-                              ),
-                            ),
-                            Text(
-                              "TRAVEL?\n",
-                              style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    color: _theme.primaryColor,
-                                    letterSpacing: .5,
-                                    fontSize: 30),
-                              ),
-                            ),
-                            Text(
-                              "THINK\n",
-                              style: GoogleFonts.roboto(
-                                textStyle: TextStyle(
-                                    color: _theme.secondaryColor,
-                                    letterSpacing: .5,
-                                    fontSize: 30),
-                              ),
-                            ),
-                            Hero(
-                              tag: 'tag',
-                              child: Material(
-                                color: Colors.transparent,
-                                child: SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.4,
-                                  child: Center(
-                                    child: Text(
-                                      "BLITZZ\n",
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.55,
+                            child: AnimationLimiter(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children:
+                                    AnimationConfiguration.toStaggeredList(
+                                  duration: const Duration(milliseconds: 1200),
+                                  childAnimationBuilder: (widget) =>
+                                      SlideAnimation(
+                                    horizontalOffset: -50.0,
+                                    child: FadeInAnimation(
+                                      child: widget,
+                                    ),
+                                  ),
+                                  children: [
+                                    Text(
+                                      "THINK\n",
+                                      style: GoogleFonts.roboto(
+                                        textStyle: TextStyle(
+                                            color: _theme.secondaryColor,
+                                            letterSpacing: .5,
+                                            fontSize: 30),
+                                      ),
+                                    ),
+                                    Text(
+                                      "TRAVEL?\n",
                                       style: GoogleFonts.roboto(
                                         textStyle: TextStyle(
                                             color: _theme.primaryColor,
@@ -76,30 +100,68 @@ class SplashScreen extends StatelessWidget {
                                             fontSize: 30),
                                       ),
                                     ),
-                                  ),
+                                    Text(
+                                      "THINK\n",
+                                      style: GoogleFonts.roboto(
+                                        textStyle: TextStyle(
+                                            color: _theme.secondaryColor,
+                                            letterSpacing: .5,
+                                            fontSize: 30),
+                                      ),
+                                    ),
+                                    Hero(
+                                      tag: 'tag',
+                                      child: Material(
+                                        color: Colors.transparent,
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.4,
+                                          child: Center(
+                                            child: Text(
+                                              "BLITZZ\n",
+                                              style: GoogleFonts.roboto(
+                                                textStyle: TextStyle(
+                                                    color: _theme.primaryColor,
+                                                    letterSpacing: .5,
+                                                    fontSize: 30),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: MediaQuery.of(context).size.height / 8),
-                      SizedBox(
-                          height: MediaQuery.of(context).size.height / 4,
-                          child: Image.asset("assets/images/blitzz_logo.png")),
-                    ],
-                  )),
-              Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width * 0.2,
-                  color: _theme.secondaryColor),
-              Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width * 0.2,
-                  color: _theme.primaryColor),
-            ],
-          ),
-        )),
+                          ),
+                          SizedBox(
+                              height: MediaQuery.of(context).size.height / 8),
+                          SizedBox(
+                              height: MediaQuery.of(context).size.height / 4,
+                              child:
+                                  Image.asset("assets/images/blitzz_logo.png")),
+                        ],
+                      )),
+                  SlideTransition(
+                    position: _offsetAnimation1,
+                    child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        color: _theme.secondaryColor),
+                  ),
+                  SlideTransition(
+                    position: _offsetAnimation2,
+                    child: Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        color: _theme.primaryColor),
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
